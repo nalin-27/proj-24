6e5522702c80414c48d0707468f01a357d00db06
nalin-27 <122039387+nalin-27@users.noreply.github.com>
2023-12-20 13:55:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file'
+
+    if file:
+        file.save('uploads/' + file.filename)  # Save the file to the uploads folder
+        return 'File uploaded successfully'
+if __name__ == "__main__":
+    app.run(debug=True)
